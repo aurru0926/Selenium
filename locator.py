a7768ec24c5bee5a6465dd83f8cd1e98ff0b3187
Alex U <ale.urru926@gmail.com>
2024-01-16 12:42:01 -0600

    Selenium

---
+++ locator.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators(object):
+    GO_BUTTON = (By.NAME, "btnK")
+    
+    
+class SearchResultPageLocators(object):
+    pass
---
+++ main.py
@@ -0,0 +1,34 @@
+import unittest
+from concurrent.futures import ThreadPoolExecutor
+from selenium import webdriver
+import page
+
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        print("setup")
+
+    def search_query(self, query):
+        with webdriver.Firefox() as driver:
+            driver.get("http://google.com")
+            mainPage = page.MainPage(driver)
+            assert mainPage.is_title_matches()
+            mainPage.search_text_element = query
+            mainPage.click_go_button()
+            driver.save_screenshot(f'screenshot_{query}.png')
+            search_result_page = page.SearchResultPage(driver)
+            assert search_result_page.is_results_found()
+
+    def test_search_python_concurrent(self):
+        search_queries = ["How many calroies in a banana", "How much HP does a 2022 Subaru wrx have?", "selenium"]
+
+        
+        max_threads = 3
+
+       
+        with ThreadPoolExecutor(max_threads) as executor:
+            executor.map(self.search_query, search_queries)
+
+if __name__ == "__main__":
+    unittest.main()
